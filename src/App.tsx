2492fe5e2aa9c89df75aa7ee928a3aaf1bf8e6c7
Artem Furman <artemfurman@gmail.com>
2024-02-03 18:57:46 +0100

    add custom DropdownMenu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { useState } from 'react'
-import SetQuantityGroup from '@/components/SetQuantityGroup'
 import Button from '@/components/Button'
+import DropdownMenu from '@/components/DropdownMenu'
+import SetQuantityGroup from '@/components/SetQuantityGroup'
+//import { ReactComponent as ChevronUp } from '@/assets/svg/chevron-up.svg';
+import ChevronUp from '@/assets/svg/chevron-up.svg?react';
+import ChevronDown from '@/assets/svg/chevron-down.svg?react';
+//import chevronUpUrl from '@/assets/svg/chevron-up.svg';
 
 function App() {
   const [quantity, setQuantity] = useState(5)
+
+  const sortOptions = ['Od A do Z', 'Od Z do A', 'Od najniższej ceny', 'Od najwyższej ceny']
+
+  const handleSort = () => {
+    return;
+  }
 
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
@@ -16,6 +27,28 @@ function App() {
         <option value="option4">Option 4</option>
       </select>
 
+      <DropdownMenu onSelect={handleSort}>
+        <DropdownMenu.Button>Sortuj</DropdownMenu.Button>
+        <DropdownMenu.List
+          className="absolute -right-2xs z-50 mt-3xs flex flex-col justify-center gap-3xs whitespace-nowrap rounded-[2rem] bg-bar
+                  p-xs text-end font-btn text-sm shadow">
+          {sortOptions.map((option, index) => (
+            <DropdownMenu.Item
+              key={index}
+              className="!hover:bg-inherit w-full !justify-end border-transparent hover:text-bar">
+              {option}
+            </DropdownMenu.Item>
+          ))}
+        </DropdownMenu.List>
+      </DropdownMenu>
+
+      <ChevronUp/>
+      <ChevronUp/>
+      <ChevronUp className='text-red-300'/>
+      <ChevronDown />
+
+      {/* <img src={chevronUpUrl} alt="Chevron Up" /> */}
+
       <SetQuantityGroup
         quantity={quantity}
         setQuantity={setQuantity}
@@ -26,7 +59,6 @@ function App() {
 
       <Button format="lg border">Start quiz</Button>
       <Button format="sm border">See my statistics</Button>
-      <Button format='round border'>+</Button>
     </main>
   )
 }
