0000e45e7f28b087cbd8c7356e7cded128450b0b
Artem Furman <artemfurman@gmail.com>
2024-02-08 11:49:28 +0100

    add timer with slide in animation

--- src/screens/PlayQuizScreen.tsx
+++ src/screens/PlayQuizScreen.tsx
@@ -1,12 +1,16 @@
 import { SetQuizStateProps } from './sharedTypes'
 import Button from '@/components/Button'
+import CountdownTimer from '@/components/CountdownTimer'
 
 export default function PlayQuizScreen({ setQuizState }: SetQuizStateProps) {
   return (
-    <div className="m-lg flex flex-col items-center justify-center gap-md rounded-[2rem] border-2 border-solid border-text bg-bg3 p-lg shadow-lg">
-      <div className="">
-        <h3>Question</h3>
-        <h3 className="text-center">3 of 15</h3>
+    <div className="relative m-lg flex flex-col items-center justify-center gap-md rounded-[2rem] border-2 border-solid border-text bg-gradient-to-r from-bg3 to-bg2 p-lg shadow-lg">
+      <CountdownTimer
+        className="flex-start slide-in-bottom absolute -top-lg right-xl -z-10 flex rounded-tl-[1rem] rounded-tr-[1rem] border-2 border-solid border-text bg-gradient-to-b from-bg2 to-bg p-xs pt-3xs text-lg"
+        initialTime={42}
+      />
+      <div className="flex flex-col gap-2xs text-center">
+        <h3>Question 3 of 15</h3>
       </div>
       <h2 className="text-center">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi consequatur perferendis
