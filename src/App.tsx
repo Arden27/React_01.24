d144f2483f58061bd80ffae56eb08c436e80a3aa
Artem Furman <artemfurman@gmail.com>
2024-02-08 10:35:25 +0100

    connect Result screen in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import CreateQuizScreen from '@/screens/CreateQuizScreen'
+import ResultScreen from './screens/ResultScreen'
 import PlayQuizScreen from './screens/PlayQuizScreen'
 
 type QuizState = 'create' | 'play' | 'result'
@@ -11,8 +12,10 @@ function App() {
     <main className="flex h-screen w-screen flex-col items-center justify-center gap-2">
       {quizState === 'play' ? (
         <PlayQuizScreen setQuizState={setQuizState} />
-      ) : (
+      ) : quizState === 'create' ? (
         <CreateQuizScreen setQuizState={setQuizState} />
+      ) : (
+        <ResultScreen setQuizState={setQuizState} />
       )}
     </main>
   )
