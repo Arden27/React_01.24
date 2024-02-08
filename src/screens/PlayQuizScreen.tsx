f1d175ea7397d1fab4198127a3a89ee9c2b00f7e
Artem Furman <artemfurman@gmail.com>
2024-02-08 12:00:20 +0100

    change answers button bg

--- src/screens/PlayQuizScreen.tsx
+++ src/screens/PlayQuizScreen.tsx
@@ -4,9 +4,9 @@ import CountdownTimer from '@/components/CountdownTimer'
 
 export default function PlayQuizScreen({ setQuizState }: SetQuizStateProps) {
   return (
-    <div className="relative m-lg flex flex-col items-center justify-center gap-md rounded-[2rem] border-2 border-solid border-text bg-gradient-to-r from-bg3 to-bg2 p-lg shadow-lg">
+    <div className="relative m-lg flex flex-col items-center justify-center gap-md rounded-[2rem] border-2 border-solid border-text bg-gradient-to-r from-bg3 to-bg2 p-lg shadow-2xl max-w-2xl">
       <CountdownTimer
-        className="flex-start slide-in-bottom absolute -top-lg right-xl -z-10 flex rounded-tl-[1rem] rounded-tr-[1rem] border-2 border-solid border-text bg-gradient-to-b from-bg2 to-bg p-xs pt-3xs text-lg"
+        className="flex-start slide-in-bottom absolute -top-lg right-xl -z-10 flex rounded-tl-[1rem] rounded-tr-[1rem] border-2 border-solid border-text bg-gradient-to-b from-bg2 to-bg p-xs pt-3xs text-lg shadow-2xl"
         initialTime={42}
       />
       <div className="flex flex-col gap-2xs text-center">
@@ -19,12 +19,12 @@ export default function PlayQuizScreen({ setQuizState }: SetQuizStateProps) {
       </h2>
       <div className="flex flex-col gap-2" onClick={() => setQuizState('result')}>
         <div className="flex flex-row gap-2">
-          <Button format="lg border fill">Answer 1</Button>
-          <Button format="lg border fill">Answer 2</Button>
+          <Button format="lg border" className='bg-bg'>Answer 1</Button>
+          <Button format="lg border" className='bg-bg'>Answer 2</Button>
         </div>
         <div className="flex flex-row gap-2">
-          <Button format="lg border fill">Answer 3</Button>
-          <Button format="lg border fill">Answer 4</Button>
+          <Button format="lg border" className='bg-bg'>Answer 3</Button>
+          <Button format="lg border" className='bg-bg'>Answer 4</Button>
         </div>
       </div>
       <Button
