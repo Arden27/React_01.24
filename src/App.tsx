4ed4400eb2d1a45244f23a50d70566f1ccce2312
Artem Furman <artemfurman@gmail.com>
2024-02-14 01:53:39 +0100

    move router to a dedicated file

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Layout } from './Layout'
-import { CreateQuizScreen } from '@/screens/CreateQuizScreen'
-import { ResultScreen } from '@/screens/ResultScreen'
-import { PlayQuizScreen } from '@/screens/PlayQuizScreen'
-import { StatisticsScreen } from '@/screens/StatisticsScreen'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <CreateQuizScreen />
-      },
-      {
-        path: '/play',
-        element: <PlayQuizScreen />
-      },
-      {
-        path: '/result',
-        element: <ResultScreen />
-      },
-      {
-        path: '/statistics',
-        element: <StatisticsScreen />
-      }
-    ]
-  }
-])
+import { RouterProvider } from 'react-router-dom'
+import { router } from '@/navigation/router'
 
 function App() {
   return <RouterProvider router={router} />
---
+++ src/navigation/router.tsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { Layout } from '@/Layout'
+import { CreateQuizScreen } from '@/screens/CreateQuizScreen'
+import { ResultScreen } from '@/screens/ResultScreen'
+import { PlayQuizScreen } from '@/screens/PlayQuizScreen'
+import { StatisticsScreen } from '@/screens/StatisticsScreen'
+
+enum ROUTES {
+  root = '/',
+  play = '/play',
+  result = '/result',
+  statistics = '/statistics'
+}
+
+export const router = createBrowserRouter([
+  {
+    path: ROUTES.root,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <CreateQuizScreen />
+      },
+      {
+        path: ROUTES.play,
+        element: <PlayQuizScreen />
+      },
+      {
+        path: ROUTES.result,
+        element: <ResultScreen />
+      },
+      {
+        path: ROUTES.statistics,
+        element: <StatisticsScreen />
+      }
+    ]
+  }
+])
