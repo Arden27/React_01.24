0e5c9fd414c0bc1d91acd2e7f46f52c3c46e9d8a
Artem Furman <artemfurman@gmail.com>
2024-02-20 15:48:58 +0100

    add redux updates to the CreateQuiz screen

--- src/screens/CreateQuizScreen.tsx
+++ src/screens/CreateQuizScreen.tsx
@@ -1,10 +1,12 @@
 import { useRef, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { ROUTES } from '@/navigation/router'
 import menuOptions from '@/data/menuOptions'
 import { Button } from '@/components/Button'
 import { DropdownMenu } from '@/components/DropdownMenu'
 import { SetQuantityGroup } from '@/components/SetQuantityGroup'
+import { setCategory, setDifficulty, setType, setTime, setNumberOfQuestions } from '@/redux/store'
 
 interface Category {
   id: number
@@ -13,14 +15,10 @@ interface Category {
 
 export function CreateQuizScreen() {
   const navigate = useNavigate()
-  const [categories, setCategories] = useState<Category[]>()
+  const dispatch = useDispatch()
+  const quizSettings = useSelector((state) => state.quiz)
+  const [categories, setCategories] = useState<Category[]>([])
   const initialLoad = useRef(true)
-  const [quizSettings, setQuizSettings] = useState({
-    category: null,
-    difficulty: null,
-    type: null,
-    time: null
-  })
 
   useEffect(() => {
     if (initialLoad.current) {
@@ -31,25 +29,39 @@ export function CreateQuizScreen() {
           setCategories(data.trivia_categories)
           console.log('loaded')
         })
-    } else {
-      console.log('second load')
     }
   }, [])
 
   const handleSelect = (label: string, selectedItem: string) => {
-    setQuizSettings(prevSettings => ({
-      ...prevSettings,
-      [label.toLowerCase()]: selectedItem
-    }));
-  };
-  
+    switch (label.toLowerCase()) {
+      case 'category':
+        dispatch(setCategory(selectedItem))
+        break
+      case 'difficulty':
+        dispatch(setDifficulty(selectedItem))
+        break
+      case 'type':
+        dispatch(setType(selectedItem))
+        break
+      case 'time':
+        dispatch(setTime(selectedItem))
+        break
+      default:
+        break
+    }
+  }
+
+  const handleSetQuantity = (quantity) => {
+    dispatch(setNumberOfQuestions(quantity))
+  }
+
   const menuOptionsWithCategories = [
     categories && {
       label: 'Category',
-      items: categories.map((category) => category.name),
+      items: categories.map((category) => category.name)
     },
     ...menuOptions
-  ];
+  ]
 
   return (
     <div className="relative m-lg flex max-w-xl flex-col items-center justify-center gap-xs rounded-[2rem] border-2 border-solid border-text bg-gradient-to-r from-bg2 to-bg3 p-lg shadow-lg">
@@ -60,6 +72,8 @@ export function CreateQuizScreen() {
         <SetQuantityGroup
           min={5}
           max={15}
+          value={quizSettings.numberOfQuestions}
+          onChange={handleSetQuantity}
           className="rounded-[2rem] border-2 border-text bg-bg3"
           classNameButtons="text-md"
           classNameInput="text-lg"
@@ -67,25 +81,26 @@ export function CreateQuizScreen() {
         <h3 className="text-lg">questions</h3>
       </div>
 
-      {menuOptionsWithCategories.map((option, index) => (
-        option && 
-        <DropdownMenu 
-        onSelect={(selectedItem) => handleSelect(option.label, selectedItem)} 
-        key={`dropdown-${index}`}
-        selected={quizSettings[option.label.toLowerCase()]}
-        >
-          <DropdownMenu.Placeholder>Choose {option.label}</DropdownMenu.Placeholder>
-          <DropdownMenu.List className="absolute -right-2xs z-50 mt-3xs flex max-h-64 flex-col gap-3xs overflow-y-auto whitespace-nowrap rounded-[2rem] bg-bar p-xs text-end font-btn text-sm shadow">
-            {option.items.map((item, itemIndex) => (
-              <DropdownMenu.Item
-                key={`${option.label}-${item}-${itemIndex}`}
-                className="w-full justify-end border-transparent hover:text-bar">
-                {item}
-              </DropdownMenu.Item>
-            ))}
-          </DropdownMenu.List>
-        </DropdownMenu>
-      ))}
+      {menuOptionsWithCategories.map(
+        (option, index) =>
+          option && (
+            <DropdownMenu
+              onSelect={(selectedItem) => handleSelect(option.label, selectedItem)}
+              key={`dropdown-${index}`}
+              selected={quizSettings[option.label.toLowerCase()]}>
+              <DropdownMenu.Placeholder>Choose {option.label}</DropdownMenu.Placeholder>
+              <DropdownMenu.List className="absolute -right-2xs z-50 mt-3xs flex max-h-64 flex-col gap-3xs overflow-y-auto whitespace-nowrap rounded-[2rem] bg-bar p-xs text-end font-btn text-sm shadow">
+                {option.items.map((item, itemIndex) => (
+                  <DropdownMenu.Item
+                    key={`${option.label}-${item}-${itemIndex}`}
+                    className="w-full justify-end border-transparent hover:text-bar">
+                    {item}
+                  </DropdownMenu.Item>
+                ))}
+              </DropdownMenu.List>
+            </DropdownMenu>
+          )
+      )}
 
       <Button format="lg border fill" className="" onClick={() => navigate(ROUTES.play)}>
         Start quiz
