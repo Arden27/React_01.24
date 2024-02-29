6c56b2c95a533c500b9623c685b550f7b41fb6d4
Artem Furman <artemfurman@gmail.com>
2024-03-01 00:26:16 +0100

    remove unused reducers

--- src/redux/slices/settings.ts
+++ src/redux/slices/settings.ts
@@ -6,28 +6,13 @@ const initialState: QuizSettings = {
   category: null,
   difficulty: null,
   type: null,
-  time: { text: '', value: '1' }
+  time: { text: '1m', value: '1' }
 }
 
 const settingsSlice = createSlice({
   name: 'settings',
   initialState,
   reducers: {
-    setNumberOfQuestions: (state, action) => {
-      state.numberOfQuestions = action.payload
-    },
-    setCategory: (state, action) => {
-      state.category = action.payload
-    },
-    setDifficulty: (state, action) => {
-      state.difficulty = action.payload
-    },
-    setType: (state, action) => {
-      state.type = action.payload
-    },
-    setTime: (state, action) => {
-      state.time = action.payload
-    },
     setSettings: (state, action) => {
       state.numberOfQuestions = action.payload.numberOfQuestions
       state.category = action.payload.category
@@ -39,14 +24,6 @@ const settingsSlice = createSlice({
   }
 })
 
-export const {
-  setNumberOfQuestions,
-  setCategory,
-  setDifficulty,
-  setType,
-  setTime,
-  resetSettings,
-  setSettings
-} = settingsSlice.actions
+export const { resetSettings, setSettings } = settingsSlice.actions
 
 export const settingsReducer = settingsSlice.reducer
