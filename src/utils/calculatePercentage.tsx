a2a498edbfb997ad01eea1bed2c8fecb746322f5
Artem Furman <artemfurman@gmail.com>
2024-03-02 15:43:17 +0100

    linter fixes

--- src/utils/calculatePercentage.tsx
+++ src/utils/calculatePercentage.tsx
@@ -1,6 +1,6 @@
 export const calculatePercentage = (correctAnswers: number, totalQuestions: number) => {
   if (totalQuestions === 0) {
-    return 0 // Return 0% if there are no questions
+    return 0
   }
   return Math.round((correctAnswers / totalQuestions) * 100)
 }
