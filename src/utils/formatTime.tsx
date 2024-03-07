228a40e53e84d39b1efbfbbf027f9cf322e21628
Artem Furman <artemfurman@gmail.com>
2024-03-07 15:34:11 +0100

    linter fixes

--- src/utils/formatTime.tsx
+++ src/utils/formatTime.tsx
@@ -1,5 +1,5 @@
 export const formatTime = (seconds: number) => {
-  const sign = seconds < 0 ? '-' : '';
-  const absoluteSeconds = Math.abs(seconds);
-  return `${sign}${String(Math.floor(absoluteSeconds / 60)).padStart(2, '0')}:${String(absoluteSeconds % 60).padStart(2, '0')}`;
+  const sign = seconds < 0 ? '-' : ''
+  const absoluteSeconds = Math.abs(seconds)
+  return `${sign}${String(Math.floor(absoluteSeconds / 60)).padStart(2, '0')}:${String(absoluteSeconds % 60).padStart(2, '0')}`
 }
